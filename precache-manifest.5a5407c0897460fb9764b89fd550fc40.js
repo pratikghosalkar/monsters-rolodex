e816b19a3d163811fa00d458d9c291ed1407205e
pratik-gh <pratik@particle41.com>
2020-05-12 17:01:54 +0530

    Updates

---
+++ precache-manifest.5a5407c0897460fb9764b89fd550fc40.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "251b7127a79847328a237ec9fce8e876",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "85ecd7e6a23898e31fd9",
+    "url": "/monsters-rolodex/static/css/main.bdf3c735.chunk.css"
+  },
+  {
+    "revision": "f7b84cd810271619eca9",
+    "url": "/monsters-rolodex/static/js/2.7d94dc6b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7d94dc6b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "85ecd7e6a23898e31fd9",
+    "url": "/monsters-rolodex/static/js/main.61035158.chunk.js"
+  },
+  {
+    "revision": "e3f9453037e5f28f8167",
+    "url": "/monsters-rolodex/static/js/runtime-main.7c0f1f0f.js"
+  }
+]);
